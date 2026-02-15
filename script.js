63e8680907607310ddaf173f9efa0242d22f81c1
NADIEZZZ <juandiegosarmiento13@gmail.com>
2026-02-14 22:13:08 -0500

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -6,7 +6,38 @@ document.addEventListener("DOMContentLoaded", function() {
   const imagenFinal = document.getElementById("imagenFinal");
 
   const thingsILove = [
-"Tu sonrisa hermosa y preciosa mi nene precioso", "me encanta cuando me dedicas videos", "me encantan las cartas que me haces", "me encanta tus gustos musicales", "me encanta tu humor y como me haces reir", "eres mi persona favorita", "tu pierna es mia jejeje", "me encantan tus gatos", "me encanta como juegas tan bien cualquier juego de peleas", "me encanta como defiendes tu opinion", "amo que le des sentido a las canciones de amor", "amo tu piel, de verdad me encanta", "me gusta que te dejes dar besos en las manos", "eres capaz de todo lo que te propongas", "me encanta que literalmente no exista una persona igual a ti", "me encantan los apodos que te tengo", "quien es mi nene precioso?", "amo extrañarte todo el tiempo", "me encanta ver peliculas contigo", "no pongo emijis en todos pq me da pereza lmao", "eres mi nene precioso", "amo cuando te puedo tomar fotos en video llamada", "me encanta que sea atrevido", "Cómo me haces sentir cuado compartimos cosas que nos gustan❤️", "Tu forma de abrazarme(me encanta sentir es extraño y escaso pero extremadamente comodo calor de tu cuerpo) 🤍", "Tu personalidad tan extravagante que me enamora 🫶", "Tu voz cuando me hablas 🎶", "Cómo me miras con tus ojos preciosos (lo que mas amo en parte lmao)🥹", "Tu corazón tan bonito(que pronto me comere) 💕", "me encanta tu cara preciosa(cada que puedo le saco foto)", "Tu risa contagiosa(tambien de mis cosas favoritas pq te empiezas a reir de la nada) 😆", "Tu manera de amar tan apasionada e intensa(propiamente unico de ti)💞", "Simplemente tú ✨"
+"Tu sonrisa hermosa y preciosa mi nene precioso",
+    "me encanta cuando me dedicas videos", 
+    "me encantan las cartas que me haces",
+    "me encanta tus gustos musicales",
+    "me encanta tu humor y como me haces reir", 
+    "eres mi persona favorita", "tu pierna es mia jejeje", 
+    "me encantan tus gatos",
+    "me encanta como juegas tan bien cualquier juego de peleas",
+    "me encanta como defiendes tu opinion",
+    "amo que le des sentido a las canciones de amor",
+    "amo tu piel, de verdad me encanta",
+    "me gusta que te dejes dar besos en las manos", 
+    "eres capaz de todo lo que te propongas",
+    "me encanta que literalmente no exista una persona igual a ti",
+    "me encantan los apodos que te tengo",
+    "quien es mi nene precioso?",
+    "amo extrañarte todo el tiempo",
+    "me encanta ver peliculas contigo",
+    "no pongo emijis en todos pq me da pereza lmao",
+    "eres mi nene precioso",
+    "amo cuando te puedo tomar fotos en video llamada",
+    "me encanta que sea atrevido",
+    "Cómo me haces sentir cuado compartimos cosas que nos gustan❤️", 
+    "Tu forma de abrazarme(me encanta sentir es extraño y escaso pero extremadamente comodo calor de tu cuerpo) 🤍",
+    "Tu personalidad tan extravagante que me enamora 🫶",
+    "Tu voz cuando me hablas 🎶",
+    "Cómo me miras con tus ojos preciosos (lo que mas amo en parte lmao)🥹",
+    "Tu corazón tan bonito(que pronto me comere) 💕", 
+    "me encanta tu cara preciosa(cada que puedo le saco foto)", 
+    "Tu risa contagiosa(tambien de mis cosas favoritas pq te empiezas a reir de la nada) 😆", 
+    "Tu manera de amar tan apasionada e intensa(propiamente unico de ti)💞",
+    "Simplemente tú ✨"
 
   ];
 
@@ -46,4 +77,5 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 });
+
 
